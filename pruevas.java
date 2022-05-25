80761d305802728de1a33768cd5948514a96d171
Ozkar17 <47155109+Ozkar17@users.noreply.github.com>
2022-05-25 16:24:05 -0600

    Add files via upload

---
+++ pruevas.java
@@ -0,0 +1,13 @@
+package Darlin;
+// @author Sanchez
+public class Ganancia {
+ public static void main(String[] args) {
+    double saldo,venta,precio;
+    System.out.print("Ingrese el saldo:");
+    saldo=entrada.real();
+    System.out.print("Ingrese precio venta:");
+    venta=entrada.real();
+    precio=venta*0.30;
+ganancia=venta+precio
+    System.out.println("Su ganancia es:"+ganancia);
+      }
